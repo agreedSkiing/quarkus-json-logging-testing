a945e245bc905b2c616dcb79205e3b0b8bee84eb
Erik Aasen <93997033+agreedSkiing@users.noreply.github.com>
2026-06-28 23:17:00 +0200

    feat: conclusion + fix of new key adder
    Signed-off-by: Erik Aasen <93997033+agreedSkiing@users.noreply.github.com>

--- src/main/java/org/acme/StackTraceInMessageProvider.java
+++ src/main/java/org/acme/StackTraceInMessageProvider.java
@@ -4,6 +4,7 @@ import io.quarkus.logging.json.runtime.JsonFormatter.JsonLogGenerator;
 import io.quarkus.logging.json.runtime.JsonProvider;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.text.MessageFormat;
 import org.jboss.logmanager.ExtLogRecord;
 
 public class StackTraceInMessageProvider implements JsonProvider {
@@ -11,7 +12,7 @@ public class StackTraceInMessageProvider implements JsonProvider {
   @Override
   public void writeTo(JsonLogGenerator generator, ExtLogRecord record)
     throws Exception {
-    String message = record.getMessage();
+    String message = getMessage(record);
     Throwable thrown = record.getThrown();
     if (thrown != null) {
       StringWriter sw = new StringWriter();
@@ -19,5 +20,27 @@ public class StackTraceInMessageProvider implements JsonProvider {
       message = message + "" + sw;
     }
     generator.add("log", message);
+  }
+
+  /**
+   * Reimplementation of getFormattedMessage since it is marked as deprecated
+   * @param record
+   * @return formatted message
+   */
+  private String getMessage(ExtLogRecord record) {
+    final String msg = record.getMessage();
+    if (msg == null) return "Empty message";
+    final Object[] parameters = record.getParameters();
+    if (parameters == null || parameters.length == 0) {
+      return msg;
+    }
+    return switch (record.getFormatStyle()) {
+      case PRINTF -> String.format(msg, parameters);
+      case MESSAGE_FORMAT -> msg.indexOf('{') >= 0
+        ? MessageFormat.format(msg, parameters)
+        : msg;
+      // should be unreachable
+      default -> msg;
+    };
   }
 }
