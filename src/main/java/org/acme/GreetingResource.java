e7ba7d6d09c106fbc96d48fa3ebcc13815f9932c
Erik Aasen <93997033+agreedSkiing@users.noreply.github.com>
2026-06-28 22:50:32 +0200

    feat: change to rest and new results with otel, log key
    Signed-off-by: Erik Aasen <93997033+agreedSkiing@users.noreply.github.com>

---
+++ src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,26 @@
+package org.acme;
+
+import io.quarkus.logging.Log;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    Log.fatalf("Yes hello from quarkus REST");
+    return "Hello from Quarkus REST";
+  }
+
+  @GET
+  @Path("/error")
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello_error() {
+    var cause = new IllegalStateException("Yes me be illegal");
+    throw new RuntimeException("FAILURE", cause);
+  }
+}
